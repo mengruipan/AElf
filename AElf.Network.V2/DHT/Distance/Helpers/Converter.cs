698caae5c6ed25f6de59e59c132c47f601b19302
Dennis Furrer <dennis.furrer@gmail.com>
2018-07-06 15:33:19 +0100

    Netv2: Comments/Notes added

--- AElf.Network.V2/DHT/Distance/Helpers/Converter.cs
+++ AElf.Network.V2/DHT/Distance/Helpers/Converter.cs
@@ -4,11 +4,21 @@ namespace AElf.Network.V2.DHT.Distance.Helpers
 {
     public class Converter
     {
+        /// <summary>
+        /// Converts a hexadecimal value to a binary string.
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns>Binary string</returns>
         public static string HexToBin(string hex)
         {
             return Convert.ToString(Convert.ToInt32(hex, 16), 2);
         }
         
+        /// <summary>
+        /// Converts a binary value to a 32-bit integer.
+        /// </summary>
+        /// <param name="binary"></param>
+        /// <returns>Integer</returns>
         public static int BinToInt(string binary)
         {
             return Convert.ToInt32(binary, 2);
--- AElf.Network.V2/DHT/Distance/XorMetric.cs
+++ AElf.Network.V2/DHT/Distance/XorMetric.cs
@@ -5,6 +5,13 @@ namespace AElf.Network.V2.DHT.Distance
 {
     public class XorMetric
     {
+        /// <summary>
+        /// This method is responsible for computing the
+        /// bitwise XOR of its operands.
+        /// </summary>
+        /// <param name="addr1Bin"></param>
+        /// <param name="addr2Bin"></param>
+        /// <returns>Binary string-formatted XOR</returns>
         public static string Calculate(string addr1Bin, string addr2Bin)
         {   
             Tuple<short[], short[]> addrArrays = Normalise(addr1Bin, addr2Bin);
@@ -15,14 +22,23 @@ namespace AElf.Network.V2.DHT.Distance
             int len = addr1.Length;
             int[] xorResult = new int[len];
 
+            // store XOR result in array
             for (int i = 0; i < (len - 1); i++)
             {
                 xorResult[i] = (addr1[i] ^ addr2[i]);
             }
             
-            return xorResult.ToString();
+            return xorResult.ToString(); // return XOR result as binary string
         }
 
+        /// <summary>
+        /// This method is responsible for converting the two
+        /// binary string-formatted addresses to arrays and
+        /// ensuring they are the same length (number of bits).
+        /// </summary>
+        /// <param name="addr1Bin"></param>
+        /// <param name="addr2Bin"></param>
+        /// <returns>Tuple containing the resulting arrays</returns>
         public static Tuple<short[], short[]> Normalise(string addr1Bin, string addr2Bin)
         {
             int addr1Len = addr1Bin.Length - 1;
@@ -33,33 +49,36 @@ namespace AElf.Network.V2.DHT.Distance
             short[] addr1 = new short[addr1Bin.Length];
             short[] addr2 = new short[addr2Bin.Length];
 
-            if ((diff = Difference(addr1Len, addr2Len)) > 0)
+            if ((diff = addr1Len - addr2Len) > 0) // if addr1 had more bits than addr2
             {
-                addr2 = PrependZeros(addr2, diff);
+                addr2 = PrependZeros(addr2, diff); // prepend zeros to addr2 so it matches addr1's length
             }
-            else if ((diff = Difference(addr1Len, addr2Len)) < 0)
+            else if ((diff = addr2Len - addr1Len) < 0) // if addr2 had more bits than addr1
             {
-                addr1 = PrependZeros(addr1, diff);
+                addr1 = PrependZeros(addr1, diff); // prepend zeros to addr1 so it matches addr2's length
             }
             
             return Tuple.Create(addr1, addr2);
         }
 
-        private static int Difference(int addr1Len, int addr2Len)
-        {
-            return addr1Len - addr2Len;
-        }
-
+        /// <summary>
+        /// This method is responsible for prepending zeros
+        /// to the input array.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="zeros"></param>
+        /// <returns>Array with the prepended zeros</returns>
         private static short[] PrependZeros(short[] input, int zeros)
         {
             short[] result = new short[input.Length + zeros];
             
+            // add each required zero to the array
             for (int i = 0; i < (zeros - 1); i++)
             {
                 result[i] = 0;
             }
             
-            Array.Copy(input, 0, result, (zeros - 1), input.Length);
+            Array.Copy(input, 0, result, (zeros - 1), input.Length); // append the input array to the result array
             
             return result;
         }
