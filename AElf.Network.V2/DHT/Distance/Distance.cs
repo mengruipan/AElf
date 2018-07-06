77fd17373b15094e03a8bf454b2797fab0645a22
Dennis Furrer <dennis.furrer@gmail.com>
2018-07-06 16:31:08 +0100

    Netv2: XOR metric simplified

--- AElf.Network.V2/DHT/Distance/Distance.cs
+++ AElf.Network.V2/DHT/Distance/Distance.cs
@@ -16,8 +16,21 @@ namespace AElf.Network.V2.DHT.Distance
             string addr1Bin = Converter.HexToBin(addr1Hex);
             string addr2Bin = Converter.HexToBin(addr2Hex);
             
-            string xorResult = XorMetric.Compute(addr1Bin, addr2Bin); // compute bitwise XOR
-            return Converter.BinToInt(xorResult); // convert & return xorResult as an integer
+            return ComputeXor(addr1Bin, addr2Bin); // compute & return distance
+        }
+
+        /// <summary>
+        /// Computes the bitwise XOR of its operands.
+        /// </summary>
+        /// <param name="addr1Bin"></param>
+        /// <param name="addr2Bin"></param>
+        /// <returns>XOR as an integer</returns>
+        private static int ComputeXor(string addr1Bin, string addr2Bin)
+        {
+            int addr1Int = Converter.BinToInt(addr1Bin);
+            int addr2Int = Converter.BinToInt(addr2Bin);
+
+            return addr1Int ^ addr2Int;
         }
     }
 }
