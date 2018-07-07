a8fb414545d014912403037da4580e3635f11697
samfrb <samos2x@outlook.com>
2018-07-07 10:08:04 +0800

    Netv2: Added simulation project

---
+++ AElf.Network.Sim/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Xsl;
+using AElf.Network.V2.DHT.Distance;
+
+namespace AElf.Network.Sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
